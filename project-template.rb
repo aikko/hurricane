4fdd0a8f9092a0c016bf8af501a911eb1b9ad5ed
jgnan1981 <jgnan1981@163.com>
2013-03-06 05:39:55 +0800

    add project_template.rb

---
+++ project-template.rb
@@ -0,0 +1,11 @@
+gem 'hurricane', '=0.0.1'
+
+generate 'hurricane:init'
+p <<TODOS
+Hurricane initialization completed.
+
+Please run: 
+rails g hurricane MODEL [attributes]
+
+to setup your user model and the admin functions
+TODOS
